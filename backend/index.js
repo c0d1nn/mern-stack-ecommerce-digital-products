7380a7f315a8e0a9e303c38c01560ba55750da4b
Muhammad Hafidz Nurrohman <hafidzn04@gmail.com>
2024-09-23 10:30:45 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,7 @@ const upload = multer({ limits: { fileSize: 50 * 1024 * 1024 } });
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://pigijo-admin.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on ${process.env.PORT} PORT`)
